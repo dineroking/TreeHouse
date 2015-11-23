09a7074c7992900dbd76a5154b64d68f8fe9a93c
Ronny Rosabal <ronnyrosabal@gmail.com>
2015-11-23 15:15:13 -0600

    finishes lessons on animsition plugin

---
+++ javascripts/animsition/js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Project: TreeHouse.
+ * By: Ronny Rosabal
+ * Date: 11/21/2015
+ * Time: 6:19 PM
+ * Content:
+ */
+
+$('.animsition').animsition({
+  inClass : 'fade-in-right-lg',
+  outClass : 'fade-out-right-lg',
+  linkElement: 'header a',
+  inDuration: '5000',
+  outDuration: '500'
+});
