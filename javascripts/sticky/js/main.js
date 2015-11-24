8b0672f65fbb13cb4fb5b64f11e11fe0d7450fd7
Ronny Rosabal <ronnyrosabal@gmail.com>
2015-11-23 20:15:20 -0600

    fisnishes the sticky plugin project

--- javascripts/sticky/js/main.js
+++ javascripts/sticky/js/main.js
@@ -6,3 +6,37 @@ $(".animsition").animsition({
   outDuration: 500
 });
 
+$('.header').sticky({
+  getWidthFrom: '.container',
+  responsiveWidth: true
+});
+
+$('.header').on('sticky-start', function(){
+
+  $('.description').html('We build <strong>great</strong> apps.');
+
+});
+
+$('.header').on('sticky-end', function(){
+
+  $('.description').html('We build apps.');
+
+});
+
+$('.work').sticky({
+  topSpacing : 64,
+  getWidthFrom: '.container',
+  responsiveWidth: true
+});
+
+$('.work').on('sticky-start', function(){
+
+  $(this).append('<a href="mailto:ronnyrosabal@aol.com" class="email-text"> Email&nbsp;Me!</a>')//TODO:remove personal email if uploaded to server
+
+});
+
+$('.work').on('sticky-end', function(){
+
+  $('.email-text').remove();
+
+});
