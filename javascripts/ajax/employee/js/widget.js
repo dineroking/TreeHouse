5f915e2fb5e17c0a4755d87a084ef6156e0c72ae
Ronny Rosabal <ronnyrosabal@gmail.com>
2015-12-11 18:38:44 -0600

    finishes employee project

---
+++ javascripts/ajax/employee/js/widget.js
@@ -0,0 +1,44 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+  if(xhr.readyState === 4){
+    console.log(typeof xhr.responseText);
+    var employees = JSON.parse(xhr.responseText);
+    console.log(typeof employees);
+    console.log(employees);
+    var statusHTML = '<ul class="bulleted">';
+    for(var i = 0; i < employees.length; i++){
+      if(employees[i].inoffice === true){
+        statusHTML += '<li class="in">';
+      }else{
+        statusHTML += '<li class="out">';
+      }
+      statusHTML += employees[i].name;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('employeeList').innerHTML = statusHTML;
+  }
+};
+xhr.open('GET', 'data/employees.json');
+xhr.send();
+
+var roomRequest = new XMLHttpRequest();
+roomRequest.onreadystatechange = function(){
+  if(roomRequest.readyState === 4){
+    var rooms = JSON.parse(roomRequest.responseText);
+    var statusHTML = '<ul class="rooms">';
+    for(var i = 0; i < rooms.length; i++){
+      if(rooms[i].available === true){
+        statusHTML += '<li class="empty">';
+      }else{
+        statusHTML += '<li class="full">';
+      }
+      statusHTML += rooms[i].room;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML;
+  }
+};
+roomRequest.open('GET', 'data/room.json');
+roomRequest.send();
